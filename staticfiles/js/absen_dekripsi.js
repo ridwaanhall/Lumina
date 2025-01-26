2f228fe3f98b6eca23b56e2d8376c2dc481154d0
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-26 17:31:12 +0700

    feat: update decryption functionality and improve user interface for Absen app

--- staticfiles/js/absen_dekripsi.js
+++ staticfiles/js/absen_dekripsi.js
@@ -3,10 +3,13 @@ const shiftValue = 3;
 var biar_jelas = dekripsi(ga_jelas, shiftValue);
 
 function showDecrypted() {
-    var mau_bolos = $('#inputcodeabsen').val();
-    var decrypted = CryptoJS.AES.decrypt(mau_bolos, biar_jelas);
-    var decrypted_2 = decrypted.toString(CryptoJS.enc.Utf8);
+    var mau_bolos = document.getElementById('inputcodeabsen').value;
+    try {
+        var decrypted = CryptoJS.AES.decrypt(mau_bolos, biar_jelas);
+        var decrypted_2 = decrypted.toString(CryptoJS.enc.Utf8);
 
-    $('#inputcodeabsen2').text(mau_bolos);
-    $('#decrypted').text(decrypted_2);
+        document.getElementById('inputcodeabsen2').value = decrypted_2; // Hanya memperbarui nilai input text
+    } catch (e) {
+        document.getElementById('inputcodeabsen2').value = "Gagal mendekripsi. Pastikan kode benar."; // Mengisi input text dengan pesan error
+    }
 }
