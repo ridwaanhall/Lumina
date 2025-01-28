dab9b2bb3f52796ce956323637319ca57ffd5cfe
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-28 21:00:13 +0700

    feat: add Open Graph URL tag generation and clean up canonical URL tag implementation

--- blog/templatetags/blog_canonical_tags.py
+++ blog/templatetags/blog_canonical_tags.py
@@ -1,14 +1,10 @@
 from django import template
-from django.conf import settings
 from django.utils.html import mark_safe
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def canonical_url(context):
-    """
-    Menghasilkan tag <link rel="canonical" href="..."> untuk URL saat ini.
-    """
     request = context.get('request')
 
     if not request:
@@ -16,5 +12,15 @@ def canonical_url(context):
 
     canonical_url = request.build_absolute_uri(request.path)
 
-    # Perubahan: Gunakan mark_safe untuk menandai output sebagai aman
     return mark_safe(f'<link rel="canonical" href="{canonical_url}" />')
+
+@register.simple_tag(takes_context=True)
+def og_url(context):
+    request = context.get('request')
+
+    if not request:
+        return ""
+
+    og_url = request.build_absolute_uri(request.path)
+
+    return mark_safe(og_url)
