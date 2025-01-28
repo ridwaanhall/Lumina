1f3e0361c3b24d12258f46b83ae340ebd78c5194
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-28 21:03:56 +0700

    feat: migrate project structure from UTY to LUMINA

--- LUMINA/wsgi.py
+++ LUMINA/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for UTY project.
+WSGI config for LUMINA project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UTY.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LUMINA.settings')
 
 application = get_wsgi_application()
 
