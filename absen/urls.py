b61d76d8f782c4997db5011eabb035f3d0b53c97
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-29 12:47:09 +0700

    feat: rename AbsenDisiniView to LuminaAppView and update URL routing

--- absen/urls.py
+++ absen/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AbsenDisiniView.as_view(), name='absen-disini'),
+    path('', views.LuminaAppView.as_view(), name='lumina-app'),
     path('terms/', views.TermsView.as_view(), name='terms'),
 ]
--- absen/views.py
+++ absen/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
-class AbsenDisiniView(View):
+class LuminaAppView(View):
     def get(self, request):
         return render(request, 'absen/absen.html')
     
