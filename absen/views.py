6c3db578246574063554d485f4c46d30017d119a
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-29 14:07:57 +0700

    refactor: remove unnecessary comments and clean up project files

--- absen/views.py
+++ absen/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views import View
 
 
-# Create your views here.
 class LuminaAppView(View):
     def get(self, request):
         try:
--- blog/blog_data.py
+++ blog/blog_data.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from django.utils import timezone
+
 
 class BlogData:
     blogs = [
