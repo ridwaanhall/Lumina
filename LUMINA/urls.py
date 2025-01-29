a3b24cd73da289980b7f1a6c44ffd87ca65c8bd0
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-29 14:18:31 +0700

    fix: reorder URL patterns for improved routing clarity Fixes #11

--- LUMINA/urls.py
+++ LUMINA/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('@ridwaanhall', views.PortfolioWebsiteView.as_view(), name='ridwaanhall'),
-    path('lanpage/', views.LandingPageView.as_view(), name='lanpage'),
-    path('<path:url>', views.CatchAllView.as_view(), name='catch_all'),
-
     path('', include('absen.urls')),
     path('blog/', include('blog.urls')),
+
+    path('@ridwaanhall', views.PortfolioWebsiteView.as_view(), name='ridwaanhall'),
+    path('lanpage/', views.LandingPageView.as_view(), name='lanpage'),
+
+    path('<path:url>', views.CatchAllView.as_view(), name='catch_all'),
 ]
