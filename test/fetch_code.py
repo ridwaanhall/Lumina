471eb7a79e08a5c737217e31086d29c97b842f7f
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-09 19:23:14 +0700

    test: update fetch_code.py to include new parameters and JSON handling for decryption API

--- test/fetch_code.py
+++ test/fetch_code.py
@@ -1,7 +1,16 @@
 import requests
+import json
 
 url = "http://127.0.0.1:8000/api/decrypt/"
-data = {"encrypted_message": "U2FsdGVkX1/UHYzZjwCyBCYsKll2ZXP7tzfFZb6mwOzqbgAuND+POcbcbwe4kT5F"}
 
-response = requests.post(url, json=data)
-print(response.json())
+data = {
+    "encrypted_message": "U2FsdGVkX18yFHUUB+bW79HoXXHHF+1e9bZBZXulYqz5sjpNS4hBir7WVLbE0K71dk69YM+99NY8LMSKajXiVQ==",
+    "new_meet_id": "300"
+}
+
+headers = {"Content-Type": "application/json"}
+
+response = requests.post(url, data=json.dumps(data), headers=headers)
+
+print(response.status_code)
+print(json.dumps(response.json(), indent=4))
