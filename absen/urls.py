a4ea99086856bbdab3078e4d4ab6d268d702120f
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-02-10 19:32:20 +0700

    refactor: remove unused views and update URL patterns

--- absen/urls.py
+++ absen/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.LuminaAppView.as_view(), name='lumina-app'),
-    path('sec/', views.LuminaSecAppView.as_view(), name='lumina-sec-app'),
-    path('simple/', views.LuminaSimpleAppView.as_view(), name='lumina-simple-app'),
     path('terms/', views.TermsView.as_view(), name='terms'),
     path('about/', views.AboutView.as_view(), name='about'),
 ]
