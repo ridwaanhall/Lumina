139e31fcd2d1fbe97980658d0f4afc383d58f224
Ridwan Halim <hi@ridwaanhall.com>
2026-03-20 04:34:08 +0700

    fix(settings): correct DEBUG config variable name; update Django to 5.2.12

--- LUMINA/settings.py
+++ LUMINA/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 KODE_AKSES = config('KODE_AKSES')
 
-DEBUG = config('DEV', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 if not DEBUG:
     ALLOWED_HOSTS = [
